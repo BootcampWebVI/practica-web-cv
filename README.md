7f6737ba13e2970d5b579337a471f6866a887c9b
Ferveloper <fermervil@gmail.com>
2018-12-12 22:51:43 +0100

    Primer maquetado general de la página

---
+++ README.md
@@ -0,0 +1,3 @@
+# Práctica: Creación de página web para currículum (CV)
+
+Trabajo en curso de creación de pagina web personal con HTML, CSS, y JavaScript.
---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+function displayMenu() {
+    var topnav = document.querySelector('.topnav');
+    if (topnav.className === "topnav") {
+      topnav.className += " responsive";
+    } else {
+      topnav.className = "topnav";
+    }
+  }
+
+// let icon = document.querySelector('.icon')
+
+// icon.on('onclick', displayMenu)
+
+    
