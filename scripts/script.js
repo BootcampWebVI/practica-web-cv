4b259457ab918374407eee6df10869fc791b4770
Ferveloper <fermervil@gmail.com>
2018-12-17 23:10:51 +0100

    Añadidos scripts de campo dinámico y límite de palabras

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,25 @@ function toggleField() {
   } else {
     document.querySelector('.referral').classList.add('hidden')
   }
+}
+
+function limitWords() {
+  let array = event.target.value.split(/[\s]+/)
+  if (array[array.length - 1] === "") array.pop()
+  let len = array.length
+	console.log("​len", len)
+  if (len >= 5 && event.keyCode == 32) {
+    if (event.keyCode == 46 || event.keyCode == 8) {
+    } else if (event.keyCode < 48 || event.keyCode > 57) {
+      event.preventDefault()
+    }
+  }
+
+  let counter = document.querySelector('.count')
+
+  if (counter.value = "") {
+    counter.innerHTML = "0"
+  } else {counter.innerHTML = len}
   
 }
 
@@ -19,6 +38,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const option = document.querySelectorAll('.radio input')
   option.forEach(i => i.addEventListener('click', toggleField))
-})
 
-    
+  const textArea = document.querySelector('textarea')
+  textArea.addEventListener('keydown', limitWords)
+  textArea.addEventListener('keyup', limitWords)
+})
