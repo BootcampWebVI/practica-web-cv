14d0fb9fdd4420c8cd37c530dd58422b596930c0
Ferveloper <fermervil@gmail.com>
2018-12-20 23:07:26 +0100

    Eliminado código innecesario

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ function setNavigation() {
   this.offsets = []
   this.sections.forEach(section => this.offsets.push(getOffset(document.querySelector(section))))
 }
-  
+
 function scrollSpy() {
   let currentOffsets = [...this.offsets, this.pageYOffset]
   let activeSection = currentOffsets.sort((a, b) => a - b).lastIndexOf(this.pageYOffset) - 1
@@ -24,22 +24,16 @@ function getOffset(element) {
 
 function smoothScroll(e) {
   // e.preventDefault()
-  document.querySelector(e.target.attributes.href.value).scrollIntoView({ behavior: 'smooth' })
-	console.log("​smoothScroll -> e.target.attributes.href.value", e.target.attributes.href.value)
+  document.querySelector(e.target.attributes.href.value).scrollIntoView({
+    behavior: 'smooth'
+  })
+  console.log("​smoothScroll -> e.target.attributes.href.value", e.target.attributes.href.value)
 }
 
 function toggleMenu() {
   if (window.innerWidth > 720) return
-  document.querySelectorAll('.sidenav li').forEach( e => e.classList.toggle('mobile-hidden'))
+  document.querySelectorAll('.sidenav li').forEach(e => e.classList.toggle('mobile-hidden'))
 }
-
-// function toggleField() {
-//   if (document.querySelector('#other').checked) {
-//     document.querySelector('.referral').classList.remove('hidden')
-//   } else {
-//     document.querySelector('.referral').classList.add('hidden')
-//   }
-// }
 
 function toggleField() {
   if (document.querySelector('#other').selected) {
@@ -70,7 +64,7 @@ function limitWords() {
 document.addEventListener('DOMContentLoaded', () => {
   this.sections = ['#home', '#who-is', '#studies', '#experience', '#about-me', '#contact']
   this.menuSections = document.querySelectorAll('.nav-section')
-  
+
   setNavigation()
   addEventListener('resize', setNavigation)
   addEventListener('scroll', scrollSpy)
@@ -80,9 +74,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const sideNav = document.querySelector('.sidenav ul')
   sideNav.addEventListener('click', toggleMenu)
-
-  // const option = document.querySelectorAll('.radio input')
-  // option.forEach(i => i.addEventListener('click', toggleField))
 
   const select = document.querySelector('select')
   select.addEventListener('change', toggleField)
